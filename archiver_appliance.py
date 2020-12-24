8b90e5646230c65e1fea7c4f290aee58f47a5ec9
Yong Hu <yhu@bnl.gov>
2020-12-23 23:07:29 -0500

    pause_pvs() is working

--- archiver_appliance.py
+++ archiver_appliance.py
@@ -27,35 +27,37 @@ from collections import OrderedDict as odict
 from epicsarchiver import ArchiverAppliance
 
 # get the Archiver's FULL hostname: localhost or hostname defined in aa.conf 
+import socket
+localhost = socket.getfqdn()
+
+import ConfigParser
+config = ConfigParser.ConfigParser()
+config.optionxform = str #keep keys as its original
+# user home directory settings will overwrite system config(/etc/...), 
+# system config will overwrite aa.conf in the current working directory
+aa_conf_user = os.path.expanduser('~/aa.conf')
+config.read(['aa.conf', '/etc/default/aa.conf', aa_conf_user])
+aaconfig_dict = {}
+sections = config.sections()
+for section in sections:
+    aaconfig_dict[section] = dict(config.items(section))
+if not aaconfig_dict:
+    print("Aborted: no aa.conf found or something wrong inside aa.conf")
+    sys.exit("Please exit python/ipython shell if the shell does not exit \
+by itself, make changes on aa.conf, then try again.")
+
 try:
-    import socket
-    localhost = socket.getfqdn()
     archiver = ArchiverAppliance(str(localhost))
     print("{}: {}".format(localhost, archiver.version))
 except:
-    #traceback.print_exc()
-    import ConfigParser
-    config = ConfigParser.ConfigParser()
-    config.optionxform = str #keep keys as its original
-    # user home directory settings will overwrite system config(/etc/...), 
-    # system config will overwrite aa.conf in the current working directory
-    aa_conf_user = os.path.expanduser('~/aa.conf')
-    config.read(['aa.conf', '/etc/default/aa.conf', aa_conf_user])
-    aaconfig_dict = {}
-    sections = config.sections()
-    for section in sections:
-        aaconfig_dict[section] = dict(config.items(section))
-    if not aaconfig_dict:
-        print("Aborted: no aa.conf found or something wrong inside aa.conf")
-        sys.exit()
-
     try:
-        archiver = ArchiverAppliance(hostname=str(aaconfig_dict['Settings']['Servername']))
+        archiver = ArchiverAppliance(hostname=str(aaconfig_dict['Host']['Name']))
         print(archiver.version)
     except:
-        print("Aborted: the Archiver server is not localhost {} and it is not \
-correctly set in aa.conf (or /etc/default/aa.conf or {})".format(localhost, aa_conf_user))
-        sys.exit()
+        print("Aborted: the Archiver server is not {} and it is not correctly \
+set in aa.conf (or /etc/default/aa.conf or {}.)\n".format(localhost, aa_conf_user))
+        sys.exit("Please exit python/ipython shell if the shell does not exit \
+by itself. Make changes on aa.conf, then try again.")
 
 '''
 archiver.abort_pv                       archiver.hostname
@@ -74,19 +76,25 @@ archiver.get_unarchived_pvs             archiver.update_pv
 archiver.get_unarchived_pvs_from_files  archiver.version
 '''
 
+import subprocess
+log_dir = os.path.expanduser("~") + "/aa-script-logs"
+subprocess.call(['mkdir', '-p', log_dir])
 
 def _log(results, file_prefix, one_line_per_pvinfo=True):
-    '''save results, which may include pv names as well as other information, 
+    '''Save results, which may include pv names as well as other information, 
     to a text file. one_line_per_pvinfo makes .txt file more easier to be 
     analyzied by other software such as Microsoft Excel 
     '''
     timestamp = time.strftime("-%Y%b%d_%H%M%S")
-    home_dir = os.path.expanduser("~")
     _file_prefix = str(file_prefix).replace(" ", "-")
-    file_name = home_dir + "/" + _file_prefix + str(timestamp)+".txt"
+    file_name = log_dir + "/" + _file_prefix + str(timestamp)+".txt"
     
     with open(str(file_name), 'w') as fd:
-        if isinstance(results[0], unicode):#string
+        if not results:
+            print("Nothing to be logged in the file %s"%file_name)
+            return
+            
+        if isinstance(results[0], unicode) or isinstance(results[0], str):
             for result in results:
                 fd.write(str(result) + "\n")
         elif isinstance(results[0], odict) or isinstance(results[0], dict):
@@ -95,13 +103,15 @@ def _log(results, file_prefix, one_line_per_pvinfo=True):
                     fd.write(str(k+"\t")+str(v)+"\t")
                     if not one_line_per_pvinfo:
                         fd.write("\n")
+                        
                 fd.write("\n")
 
     print("{} PVs have been written to the file {}\n".format(len(results), file_name))
 
 
-def _get_pvnames(results, sort=True):
-    '''get PV names only, no other information'''
+def _get_pvnames(results, sort=True, do_return=True):
+    '''Get PV names only, no other information. 
+    To avoid verbose output when using python/ipython shell, set do_return=False'''
     if not results:
         print("No PVs found\n")
         return
@@ -119,8 +129,9 @@ def _get_pvnames(results, sort=True):
     for pv in pvs_4print:
         print(pv)
     print("...\n%d PVs\n"%len(pvnames))
-
-    return pvnames
+    
+    if do_return:
+        return pvnames
 
 
 def get_pvnames_from_file(filename):
@@ -139,66 +150,12 @@ def get_pvnames_from_file(filename):
     return list(pvnames)
 
 
-def report_pvnames(pattern='*', regex='*', limit=500, return_pvnames=False):
-    '''Get pv names (up to the max. number 'limit') by 'pattern' and 'regex', then 
-    write them to a file. If return_pvnames is True, then report & return pvnames
-    '''
-    pvs = archiver.get_all_pvs(pv=pattern, regex=regex, limit=limit)
-    
-    if pattern == "*":
-        file_prefix = "all pvnames"
-    else:
-        file_prefix = str(pattern) + "-" + str(limit) + '-pvnames' 
-    _log(pvs, file_prefix)
-    
-    if return_pvnames:
-        return _get_pvnamess(pvs)
-    else:
-        _get_pvnames(pvs) # just report pvnames    
-
-
-def report_all_pvnames():
-    '''Get all pv names in the Archiver and write them to a file'''
-    report_pvnames(pattern="*", regex="*", limit=-1, return_pvnames=False)
-
-
-def report_waveform_pvs(log_file_info=True, return_pvnames=False):
-    ''' Get a list of dicts of waveform PVs that are currently being archived,
-    then try to log .pb file information (name, size) and pv names  
-    '''
-    results = archiver.get_archived_waveforms()
-    pvnames = _get_pvnames(results)
-    _log(pvnames, "all waveform pvnames");
-
-    if log_file_info:
-        pvnames = [dic['pvName'] for dic in results]
-        pvnames.sort()
-        info = get_pvs_file_info(pvnames)
-        _log(info, "waveform file info")
-
-    if return_pvnames:
-        return pvnames
-
-
-def report_storage_rate(limit=1000, return_pvnames=False):
-    '''Get a list of dicts of PVs sorted by descending storage rate.
-    '''
-    results = archiver.get_storage_rate_report(limit=limit) 
-    _log(results, "storage rate");
-
-    if return_pvnames:
-        return _get_pvnames(results, sort=False) # DO NOT sort 
-    else:
-        _get_pvnames(results, sort=False)
-
-
 def get_pvs_file_info(pvnames, report_zero_size=True, only_report_total_size=True,
     only_report_current_year_file=True, lts_path='/DATA/lts/ArchiverStore/'):
     '''- Get archived data file name and file size for each pvname in pvnames
     pv = "SR-RF{CFD:2-Cav}E:I"; relative_path = 'SR/RF/CFD/2/Cav/E/I'
     pb_file: /DATA/lts/ArchiverStore/SR/RF/CFD/2/Cav/E/I:2016.pb
     '''
-    year=time.strftime("%Y")
     pvs_file_info = []
     for pvname in pvnames:
         pv_file_info = odict()
@@ -232,4 +189,126 @@ def get_pvs_file_info(pvnames, report_zero_size=True, only_report_total_size=Tru
 
     return pvs_file_info
             
+
+'''
+Each of the following report_* functions does a few common things: reporting
+debug information, logging pv names (or more details on pvs), returning pv names
+if do_return is explicitly set True. So, each function does more than just 'report'.
+'''   
+def report_pvnames(pattern='*', regex='*', limit=500, do_return=False):
+    '''Get pv names (number of pv names <= 'limit') based on 'pattern' and 'regex', 
+    then write them to a file. If do_return is True, then report & return pvnames
+    '''
+    pvs = archiver.get_all_pvs(pv=pattern, regex=regex, limit=limit)
+    
+    if pattern == "*":
+        file_prefix = "all pvnames"
+    else:
+        file_prefix = str(pattern) + "-" + str(limit) + '-pvnames' 
+    _log(pvs, file_prefix)
+    return _get_pvnames(pvs, do_return=do_return)     
+
+
+def report_all_pvnames(do_return=False):
+    '''Get all pv names in the Archiver and write them to a file'''
+    return report_pvnames(pattern="*", regex="*", limit=-1, do_return=do_return)
+
+
+def report_waveform_pvs(log_file_info=True, do_return=False):
+    ''' Get a list of dicts of waveform PVs that are currently being archived,
+    then try to log .pb file information (name, size) and pv names  
+    '''
+    results = archiver.get_archived_waveforms()
+    pvnames = _get_pvnames(results)
+    _log(pvnames, "all waveform pvnames");
+
+    if log_file_info:
+        info = get_pvs_file_info(pvnames)
+        _log(info, "waveform file info")
+
+    if do_return:
+        return pvnames
+
+
+def report_storage_rate(limit=1000, do_return=False):
+    '''Get a list of dicts of PVs sorted by descending storage rate.
+    '''
+    results = archiver.get_storage_rate_report(limit=limit) 
+    _log(results, "storage rate");
+    return _get_pvnames(results, sort=False, do_return=do_return) # DO NOT sort 
+
+
+def report_never_connected_pvs(do_return=False):
+    '''Get never connected pvs, the same as the web "PV's that may not exist"
+    We can not pause never connected pvs, we can abort them.
+    '''
+    results = archiver.get_never_connected_pvs()
+    pvnames = _get_pvnames(results)    
+    _log(pvnames, "never connected pvnames")
+    _log(results, "never connected pvs details")
+    
+    if do_return:
+        return pvnames    
+ 
+        
+def report_currently_disconnected_pvs(do_return=False):
+    '''Get currently disconnected pvs, meaning they were archived at least once'''
+    results = archiver.get_currently_disconnected_pvs()
+    pvnames = _get_pvnames(results)    
+    _log(pvnames, "currently disconnected pvnames")
+    _log(results, "currently disconnected pvs details")
+    
+    if do_return:
+        return pvnames  
+
+
+def _get_authentication():
+    try: 
+        userID = os.popen('whoami').read()[:-1] 
+        if userID not in aaconfig_dict["Superusers"]["account"]:
+            print("You do not have permission to do this kind of action on AA")
+            sys.exit("Please exit python/ipython shell if the shell does not exit \
+by itself. Ask for permission to work on Archiver, then try again.")
+
+    except KeyError:
+        pass # no authentication if no aaconfig_dict["Superusers"]["*"]
+        
+    
+def pause_pvs(pvnames_src=None):
+    '''Pause each pv in 'pvnames_src' if permission is allowed.
+    pvnames_src(source where we get pvnames): 
+    1) default is None: pvnames are currently disconnected PVs;
+    2) a list of pv names: i.e. ['pv1', 'pv2'];
+    3) filename: i.e. 'pause_pvs.txt', pv names should be listed as one column
+    '''
+    _get_authentication()
+    
+    if pvnames_src is None:
+        pvnames = report_currently_disconnected_pvs(do_return=True)
+    elif isinstance(pvnames_src, list):
+        pvnames = pvnames_src
+    else:
+        pvnames = get_pvnames_from_file(pvnames_src)
+    
+    pvnames = _get_pvnames(pvnames) # just for printing some debug messages
+    answer = raw_input("Do you really want to pause those PVs? Type yes or no: ")
+    if answer.upper() != "YES":
+        print("Quit. No PVs paused.")
+        return 
+        
+    results = []
+    paused_pvnames = []
+    for pvname in pvnames:
+        result = archiver.pause_pv(pvname) 
+        results.append(result)
+        try:
+            if result['status'] == 'ok':
+                print("Successfully paused %s."%pvname)
+                paused_pvnames.append(pvname)
+        except:
+            print("%s is already paused or it is not in the Archiver."%pvname)
+
+    _log(results, "pause_pv detailed results")
+    _log(paused_pvnames, "paused pvnames")
+
 
