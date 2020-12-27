e2bee78de2727e817596acb82edad4971eafa640
Yong Hu <yhu@bnl.gov>
2020-12-27 16:08:18 -0500

    improvements on a few functions: get_pvs_file_info(), etc.

--- archiver_appliance.py
+++ archiver_appliance.py
@@ -1,29 +1,14 @@
 # -*- coding: utf-8 -*-
-'''
-The purpose of this module is to provide a higher level of functions, by utilizing 
-the class methods implemented in epicsarchiver.py, to more easily manipulate AA
-
-Basic Usage::
-    >>> import archiver_appliance as aa
-    >>> import datetime
-    >>> archappl = ArchiverAppliance('archiver-01.tn.esss.lu.se')
-    >>> print(archappl.version)
-    >>> archappl.get_pv_status(pv='BPM*')
-    >>> _end = datetime.utcnow()
-    >>> df = archappl.get_data('my:pv', start='2018-07-04 13:00', end=_end)
-        
-'''
+'''The purpose of this module is to provide a higher level of functions, utilizing 
+the class methods implemented in epicsarchiver.py, to more easily manage AA.'''
 
 from __future__ import print_function
 import os
 import sys
-from datetime import datetime
 import time
-import traceback
 import re
 import glob
 from collections import OrderedDict as odict
-
 from epicsarchiver import ArchiverAppliance
 
 # get the Archiver's FULL hostname: localhost or hostname defined in aa.conf 
@@ -59,22 +44,6 @@ set in aa.conf (or /etc/default/aa.conf or {}.)\n".format(localhost, aa_conf_use
         sys.exit("Please exit python/ipython shell if the shell does not exit \
 by itself. Make changes on aa.conf, then try again.")
 
-'''
-archiver.abort_pv                       archiver.hostname
-archiver.archive_pv                     archiver.identity
-archiver.archive_pvs                    archiver.info
-archiver.archive_pvs_from_files         archiver.mgmt_url
-archiver.data_url                       archiver.pause_pv
-archiver.delete_pv                      archiver.pause_rename_resume_pv
-archiver.get                            archiver.post
-archiver.get_all_expanded_pvs           archiver.rename_pv
-archiver.get_all_pvs                    archiver.rename_pvs_from_files
-archiver.get_data                       archiver.request
-archiver.get_pv_status                  archiver.resume_pv
-archiver.get_pv_status_from_files       archiver.session
-archiver.get_unarchived_pvs             archiver.update_pv
-archiver.get_unarchived_pvs_from_files  archiver.version
-'''
 
 import subprocess
 log_dir = os.path.expanduser("~") + "/aa-script-logs"
@@ -85,15 +54,15 @@ def _log(results, file_prefix, one_line_per_pvinfo=True, **kargs):
     to a text file. one_line_per_pvinfo makes .txt file more easier to be 
     analyzied by other software such as Microsoft Excel 
     '''
-    timestamp = time.strftime("-%Y%b%d_%H%M%S")
-    _file_prefix = str(file_prefix).replace(" ", "-")
-    file_name = log_dir + "/" + _file_prefix + str(timestamp)+".txt"
-    
-    with open(str(file_name), 'w') as fd:
-        if not results:
-            print("Nothing to be logged in the file %s"%file_name)
-            return
+    if not results:
+        print("Nothing to be logged for %s"%file_prefix)
+        return
             
+    timestamp = str(time.strftime("-%Y%b%d_%H%M%S"))
+    prefix = str(file_prefix).replace(" ", "-")
+    file_name = log_dir + "/" + prefix +'-'+str(len(results)) + timestamp+".txt"
+
+    with open(str(file_name), 'w') as fd:
         if isinstance(results[0], unicode) or isinstance(results[0], str):
             for result in results:
                 fd.write(str(result) + "\n")
@@ -137,67 +106,87 @@ def _get_pvnames(results, sort=True, do_return=True, **kargs):
         return pvnames
 
 
-def get_pvnames_from_file(filename):
+def get_pvnames_from_file(filename='pvlist.txt'):
     '''pvnames in 'filename' should be listed as one column'''
     with open(filename, "r") as fd:
         lines = fd.readlines()
-        pvnameList = []
+        pvname_list = []
         for line in lines:
+            line = str(line).strip() # do the stip() first
+            # Remove empty lines and lines that start with "#"            
             if line.startswith("#") or line == "":
-            # Remove empty lines and lines that start with "#"
                 continue
-            pvnameList.append(str(line).strip())
+            pvname_list.append(line)
             
-    pvnames = set(pvnameList) # remove duplicated PVs
+    pvnames = list(set(pvname_list)) # remove duplicated PVs
+    pvnames.sort()
     print("get %d PVs from %s"%(len(pvnames), filename))
-    return list(pvnames)
+    return pvnames
 
 
-def get_pvs_file_info(pvnames, report_zero_size=True, only_report_total_size=True,
-    only_report_current_year=True, lts_path='/DATA/lts/ArchiverStore/', **kargs):
-    '''- Get archived data file name and file size for each pvname in pvnames
-    pv = "SR-RF{CFD:2-Cav}E:I"; relative_path = 'SR/RF/CFD/2/Cav/E/I'
-    pb_file: /DATA/lts/ArchiverStore/SR/RF/CFD/2/Cav/E/I:2016.pb
-    '''
+def get_pvs_file_info(pvnames,only_report_total_size=True,report_current_year=True,
+                      lts_path=str(aaconfig_dict["Lts"]["Path"]), **kargs):
+    '''- Get archived data file name and file size for each pvname in pvnames.
+    pvname = "SR-RF{CFD:2-Cav}E:I"; relative_path = 'SR/RF/CFD/2/Cav/E/I';
+    pb_file: lts_path/SR/RF/CFD/2/Cav/E/I:2016.pb. '''
+    if not os.path.isdir(lts_path):
+        print("Aborted: the long-term storage(lts) path '{}' seems not correct,\
+please reconfigure it in aa.conf".format(lts_path))
+        sys.exit("Please exit python/ipython shell if the shell does not exit \
+by itself. Make changes on aa.conf, then try again.")
+
     pvs_file_info = []
+    zero_size_pvnames = []
     for pvname in pvnames:
         pv_file_info = odict()
         total_GB = 0.0
         file_names = ""
-        relative_path = re.sub('[:{}-]', '/', pvname)
-        full_path = lts_path + str(relative_path)
+        # replace the special characters, ':', '{', '}', '-', with '/'
+        relative_path = re.sub('[:{}-]', '/', pvname) #this is specific for NSLS-2
+        #relative_path = re.sub('[char_set]', '/', pvname)
+        full_path = lts_path + '/' + str(relative_path)
         
         for pb_file in glob.glob(full_path+'*'):
             year = "".join("".join(pb_file.rsplit(full_path+':'))).rsplit('.pb')[0]
-            size_GB = round(1.0*os.path.getsize(pb_file)/(1024**3))
+            size_GB = round(1.0*os.path.getsize(pb_file)/(1024**3), 9)
             total_GB += size_GB
             if not only_report_total_size:
-                pv_file_info[pvname+'\t'+year] = size_GB # GB per year
-            if only_report_current_year:
+                pv_file_info[pvname+'('+year+')'] = size_GB # GB per year
+            if report_current_year:
                 if year == time.strftime("%Y"):
                     file_names = pb_file
                     pv_file_info[pvname+" "+year] = size_GB
             else:
                 file_names += (pb_file + "    ")
 
-        if report_zero_size:
-            if not total_GB: 
-                print("%s\t has no .pb files"%pvname)
+        if not total_GB: # zero-size
+            zero_size_pvnames.append(pvname)
                 
-        pv_file_info[pvname] = total_GB # total file size (GB) for  pv 
+        pv_file_info[pvname+'(total)'] = total_GB # total file size (GB) for  pv 
         pv_file_info[pvname+'(path)'] = full_path
         pv_file_info[pvname+'(file_names)'] = file_names
 
         pvs_file_info.append(pv_file_info)
 
-    return pvs_file_info
+    return (pvs_file_info, zero_size_pvnames)
             
 
-def _report(report_type="", **kargs):
+def report(report_type="", **kargs):
     '''A generic function which does more then just 'reporting something': it 
     gets data from the Archiver, parses those data to get pv names, does all 
-    kinds of logs, and finally returns pv names if needed. '''
-    #print(kargs)
+    kinds of logs, and finally returns pv names if needed. 
+    Supported keyword arguments:
+      1) do_return=True: return pv names only; can be used for all report_*(); 
+      2) log_file_info=True: write pvs' file info (name, size, etc.) to a file;
+      3) filename='your-customized-text-file': used for report_pvs_from_file();
+      4) pattern=something-like-'SR:C03-BI*': used for report_pvs();  
+      5) regex='*': used for report_pvs();  
+      6) limit=max-number-of-pvs: for get_storage_rate_report() and report_pvs(); 
+      7) one_line_per_pvinfo: if False, key & value per line in the log file;
+      8) sort: if False, pv names are not sorted;
+      And the following can be used if log_file_info=True: lts_path,
+      only_report_total_size, report_current_year. '''
+    print("keyword arguments: {}".format(kargs))
     if report_type == 'never connected':
         results =  archiver.get_never_connected_pvs()
     elif report_type == 'currently disconnected':
@@ -210,73 +199,82 @@ def _report(report_type="", **kargs):
         results = archiver.get_archived_waveforms()
     elif report_type == 'search':
         results = archiver.get_all_pvs(pv=kargs.pop('pattern', '*'), 
-            regex=kargs.pop('regex', '*'), limit=kargs.pop('limit', 500))
+            regex=kargs.pop('regex', '*'), limit=kargs.pop('limit', 1000))
     elif report_type == 'pvs from file':
         results = get_pvnames_from_file(kargs.pop('filename', 'pvlist.txt'))
                  
     pvnames = _get_pvnames(results, **kargs)    
     _log(pvnames, report_type + " pvnames", **kargs)
-    _log(results, report_type + " details", **kargs)
+    
+    if len(results) > 0:
+        if isinstance(results[0], dict):
+            _log(results, report_type + " details", **kargs)
     
     if kargs.pop('log_file_info', False):
         info = get_pvs_file_info(pvnames, **kargs)
-        _log(info, report_type + " pvs file info", **kargs)      
+        _log(info[0], report_type + " pvs file info", **kargs)   
+        if len(info[1]) > 0: 
+            zero_size_pvnames = info[1]
+            zero_size_pvnames.sort()
+            _log(zero_size_pvnames, report_type + " zero-size pvnames", **kargs)  
     
     if kargs.pop('do_return', False):
         return pvnames
 
       
-#def report_pvs(pattern='*', regex='*', limit=500, **kargs): #this is wrong
+#def report_pvs(pattern='*', regex='*', limit=1000, **kargs):#this does not work
 def report_pvs(**kargs):  
     '''Report pvs (number of pvs <= 'limit') based on 'pattern' and 'regex'. 
-    To return pv names, use do_return=True.'''
-    return _report('search', pattern='*', regex='*', limit=500, **kargs)     
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report('search', pattern='*', regex='*', limit=1000, **kargs)     
 
 
 def report_all_pvs(**kargs):
-    '''Report all pvs in the Archiver. To return pv names, use do_return=True.'''
-    return _report('search', pattern="*", regex="*", limit=-1, **kargs)
+    '''Report all pvs in the Archiver. 
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report('search', pattern="*", regex="*", limit=-1, **kargs)
    
     
 def report_pvs_from_file(**kargs):
     '''Report pvs which are listed as one column in a file. 
-    To return pv names, use do_return=True.'''
-    return _report("pvs from file", filename='pvlist.txt', **kargs)   
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report("pvs from file", filename='pvlist.txt', **kargs)   
     
     
 def report_waveform_pvs(**kargs):
     '''Report waveform PVs that are currently being archived. 
-    To return pv names, use do_return=True.'''
-    return _report(report_type='waveform', log_file_info=True, **kargs)
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report(report_type='waveform', log_file_info=True, **kargs)
 
 
 def report_storage_rate(**kargs):
     '''Report PVs sorted by descending storage rate. 
-    To return pv names, use do_return=True.'''
-    return _report(report_type='storage rate', sort=False, **kargs) 
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report(report_type='storage rate', sort=False, **kargs) 
 
 
 def report_never_connected_pvs(**kargs):
     '''Report never connected pvs, as "PV's that may not exist" on the web. 
-    To return pv names, use do_return=True.'''
-    return _report(report_type='never connected', **kargs);
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report(report_type='never connected', **kargs)
  
         
 def report_currently_disconnected_pvs(**kargs):
     '''Report currently disconnected pvs, meaning they used to be connected.
-    To return pv names, use do_return=True.'''
-    return _report(report_type='currently disconnected', **kargs);
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report(report_type='currently disconnected', **kargs)
 
 
 def report_paused_pvs(**kargs):
-    '''Report currently paused pvs. To return pv names, use do_return=True.'''
-    return _report(report_type='paused', **kargs);
+    '''Report currently paused pvs. 
+    See the function 'report' (type help(aa.report)) for all keyword arguments.'''
+    return report(report_type='paused', **kargs)
         
         
 def _get_authentication():
     try: 
         userID = os.popen('whoami').read()[:-1] 
-        if userID not in aaconfig_dict["Superusers"]["account"]:
+        if userID not in aaconfig_dict["Superusers"]["Account"]:
             print("You do not have permission to do this kind of action on AA")
             sys.exit("Please exit python/ipython shell if the shell does not exit \
 by itself. Ask for permission to work on Archiver, then try again.")
